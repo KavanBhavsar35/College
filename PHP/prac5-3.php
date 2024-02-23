5fd5649b35ef8aff8eb2e5a01c9203d71d68c5d2
Kavan Bhavsar <kvbhavsar35@gmail.com>
2024-02-23 14:38:06 +0530

    prac 5

--- PHP/prac5-3.php
+++ PHP/prac5-3.php
@@ -1,1 +1,19 @@
-<?
+<?php 
+    $arr1 = array(
+        array(1,2,3),
+        array(4,5,6),
+        array(7,8,9)
+    );
+    $arr2 = array(
+        array(7,8,9),
+        array(4,5,6),
+        array(1,2,3)
+    );
+    $arr3 = array();
+    for ($i = 0; $i < count($arr1); $i++) {
+        for ($j = 0; $j < count($arr1); $j++) {
+            $arr3[$i][$j] = $arr1[$i][$j] * $arr2[$i][$j];
+        }
+    }
+    print_r($arr3);
+?>
