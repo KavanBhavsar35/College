e7c959f1b0d8d65d9aefee1f4d973e777276e295
Kavan Bhavsar <kvbhavsar35@gmail.com>
2024-03-09 15:39:50 +0530

    prac6-2 cnahges

--- PHP/prac6-2.php
+++ PHP/prac6-2.php
@@ -1,22 +1,7 @@
 <?php
-function isLowercase($str) {
-    return ctype_lower($str);
-}
-
-function reverseString($str) {
-    return strrev($str);
-}
-
-function removeSpaces($str) {
-    return str_replace(' ', '', $str);
-}
-
-function replaceWord($str, $word, $replace) {
-    return str_replace($word, $replace, $str);
-}
 
 $string = "Hello, World!";
-echo isLowercase($string) ? "String $string is lowercase\n" : "String $string is not lowercase\n" . "</br></br></br>";
-echo "Reversed String: " . reverseString($string) . "\n" . "</br></br></br>";
-echo "String without spaces: " . removeSpaces($string) . "\n" . "</br></br></br>";
-echo "String with 'World' replaced by 'Universe': " . replaceWord($string, "World", "Universe") . "\n" . "</br></br></br>";
+echo ctype_lower($str) ? "String $string is lowercase\n" : "String $string is not lowercase\n" . "</br></br></br>";
+echo "Reversed String: " . strrev($string) . "</br></br></br>";
+echo "String without spaces: " . str_replace(' ', '', $string) . "</br></br></br>";
+echo "String with 'World' replaced by 'Universe': " . str_replace("World", "Universe", $string) . "\n" . "</br></br></br>";
